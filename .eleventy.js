206550a18b82311b50390472cb7b50da87e527a9
Dino-996 <davidesabia22@gmail.com>
2026-02-10 21:40:09 +0100

    eleventy init and configuration

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+      includes: "_includes"
+    }
+  };
+  
+};
